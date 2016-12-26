8ca1ea2ca0fd1b33743e9aa92c3422fddba9736d
root <teocollin1995@gmail.com>
2016-12-25 19:08:20 -0500

    slack config

---
+++ slackbot_settings.py
@@ -0,0 +1,19 @@
+#Slack Config
+team_name = ''
+bot_name = ''
+API_TOKEN = ""
+cookie = ''
+fontname = "DejaVuSerif.ttf"
+DEFAULT_REPLY = "Sorry but I didn't understand you"
+ERRORS_TO = 'username'
+PLUGINS = []
+
+
+#latex config
+command = "/tex"
+webhook_url = ""
+png = "/usr/bin/dvipng"
+tex = "/usr/bin/latex"
+template = "pathToTemplate"
+
+
