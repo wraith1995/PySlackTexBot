8bb1c819d2446a29e2fd07be838499e92338c390
root <teocollin1995@gmail.com>
2016-12-25 19:16:18 -0500

    bot skeleton

---
+++ texbot/plugins.py
@@ -0,0 +1,16 @@
+from slackbot.bot import respond_to
+import re
+import json
+
+
+@respond_to('github', re.IGNORECASE)
+def github():
+    attachments = [
+    {
+        'fallback': 'Fallback text',
+        'author_name': 'Author',
+        'author_link': 'http://www.github.com',
+        'text': 'Some text',
+        'color': '#59afe1'
+    }]
+    message.send_webapi('', json.dumps(attachments))
